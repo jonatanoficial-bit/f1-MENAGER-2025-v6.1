30abd9ed427ce460a76dd9e535edea7ede9fdbc4
jonatanoficial-bit <jonatanoficial@gmail.com>
2025-12-07 09:42:02 -0300

    data.js

--- data.js
+++ data.js
@@ -74,3 +74,98 @@ const GAME_DATA = {
                 suspension: [5, 7]
             }
         },
+{
+            id: "china",
+            name: "Chinese Grand Prix",
+            country: "China",
+            flag: "assets/flags/china.png",
+            date: "06 APR 2025",
+            svg: "assets/tracks/china.svg.svg",
+            laps: 56,
+            lengthKm: 5.451,
+            tyreWear: "high",
+            fuelUse: "medium",
+            rainChance: 0.25,
+            setupWindow: {
+                frontWing: [6, 8],
+                rearWing: [6, 8],
+                rideHeight: [4, 6],
+                suspension: [5, 6]
+            }
+        },
+        {
+            id: "japan",
+            name: "Japanese Grand Prix",
+            country: "Japan",
+            flag: "assets/flags/japan.png",
+            date: "13 APR 2025",
+            svg: "assets/tracks/japan.svg.svg",
+            laps: 53,
+            lengthKm: 5.807,
+            tyreWear: "very_high",
+            fuelUse: "high",
+            rainChance: 0.42,
+            setupWindow: {
+                frontWing: [7, 9],
+                rearWing: [7, 9],
+                rideHeight: [5, 7],
+                suspension: [6, 7]
+            }
+        },
+        {
+            id: "miami",
+            name: "Miami Grand Prix",
+            country: "USA",
+            flag: "assets/flags/usa.png",
+            date: "04 MAY 2025",
+            svg: "assets/tracks/miami.svg.svg",
+            laps: 57,
+            lengthKm: 5.410,
+            tyreWear: "low",
+            fuelUse: "high",
+            rainChance: 0.30,
+            setupWindow: {
+                frontWing: [4, 6],
+                rearWing: [4, 6],
+                rideHeight: [3, 5],
+                suspension: [4, 5]
+            }
+        },
+        {
+            id: "imola",
+            name: "Emilia Romagna Grand Prix",
+            country: "Italy",
+            flag: "assets/flags/italy.png",
+            date: "18 MAY 2025",
+            svg: "assets/tracks/imola.svg.svg",
+            laps: 63,
+            lengthKm: 4.909,
+            tyreWear: "medium",
+            fuelUse: "medium",
+            rainChance: 0.33,
+            setupWindow: {
+                frontWing: [6, 8],
+                rearWing: [6, 8],
+                rideHeight: [4, 6],
+                suspension: [5, 6]
+            }
+        },
+        {
+            id: "monaco",
+            name: "Monaco Grand Prix",
+            country: "Monaco",
+            flag: "assets/flags/monaco.png",
+            date: "25 MAY 2025",
+            svg: "assets/tracks/monaco.svg.svg",
+            laps: 78,
+            lengthKm: 3.337,
+            tyreWear: "low",
+            fuelUse: "low",
+            rainChance: 0.36,
+            setupWindow: {
+                frontWing: [8, 11],
+                rearWing: [9, 11],
+                rideHeight: [6, 8],
+                suspension: [6, 8]
+            }
+        },
